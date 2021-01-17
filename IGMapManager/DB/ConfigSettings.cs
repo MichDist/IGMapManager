296ee32a23f83bbd937ad53173d0423c5fa3195c
MichDist <51273200+MichDist@users.noreply.github.com>
2021-01-17 11:50:04 +0100

    New path methods

--- IGMapManager/DB/ConfigSettings.cs
+++ IGMapManager/DB/ConfigSettings.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +11,33 @@ namespace IGMapManager.DB
     {
         public static Boolean setGameDirPath(string pPath, string pGame)
         {
-            string str = @"Data Source=C:\Projekte\IGMapManager\test.db";
-            var connection = new SqliteConnection(str);
-
+            string str = @"Data Source=C:\Projekte\IGMapManager\map_db.db";
+            var connection = new SQLiteConnection(str);
             connection.Open();
+
+            var command = new SQLiteCommand(connection);
+            command.CommandText = "INSERT INTO paths(game, path, repo) VALUES (@pGame, @pPath, @pRepo)";
+            command.Parameters.AddWithValue("@pGame", pGame);
+            command.Parameters.AddWithValue("@pPath", pPath);
+            command.Parameters.AddWithValue("@pRepo", "no");
+
+            command.Prepare();
+            command.ExecuteNonQuery();
 
             return true;
         }
 
         public static string getGameDirPath(string pGame)
         {
+            string str = @"Data Source=C:\Projekte\IGMapManager\map_db.db";
+            var connection = new SQLiteConnection(str);
+            connection.Open();
 
+            var command = new SQLiteCommand(connection);
+            command.CommandText = "SELECT path FROM paths WHERE @pGame = game AND repo = no";
+            command.Parameters.AddWithValue("@pGame", pGame);
+            command.Prepare();
+            command.ExecuteReader();
 
             return "";
         }
